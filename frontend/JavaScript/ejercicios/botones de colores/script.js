f41a229ec7b6461859254449e238416424c40ce1
Elurr <jonatikkan@gmail.com>
2022-11-24 13:49:25 +0100

    clase del jueves 24-11

--- frontend/JavaScript/ejercicios/botones de colores/script.js
+++ frontend/JavaScript/ejercicios/botones de colores/script.js
@@ -1,10 +1,7 @@
-
-
-
 // Función sencilla
 function colorChange(color) {
     document.body.style.background = color;
-    console.log('has pulsado un boton');
+//     console.log('has pulsado un boton');
 }
 function colorReset() {
     document.body.style.background = 'linear-gradient(90deg,rgb(41, 56, 97), black)';
