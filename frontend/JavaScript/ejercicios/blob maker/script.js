531668cf88c403d977a6bad368d59c83399386cb
Elurr <jonatikkan@gmail.com>
2022-11-25 12:39:57 +0100

    blob funcional

--- frontend/JavaScript/ejercicios/blob maker/script.js
+++ frontend/JavaScript/ejercicios/blob maker/script.js
@@ -2,24 +2,45 @@
 const outputcode = document.querySelector('.output-code');
 
 // array con los sliders
-const slider = document.querySelectorAll('input[type="range"]');
+const sliders = document.querySelectorAll('input[type="range"]');
 
 // recorremos el array para asignar un evento a cada slider
-slider.forEach(function(slider) {
+sliders.forEach(function(slider) {
     slider.addEventListener('change', createBlob);
 });
 
 
 // array con los input numéricos
-const input = document.querySelectorAll('input[type="number"]');
+const inputs = document.querySelectorAll('input[type="number"]');
 
 // asignamos eventos a los inputs
-input.forEach(function(input) {
+inputs.forEach(function(input) {
     input.addEventListener('change', createBlob);
-})
+});
 
 
 // función que crea un blob nuevo cada vez que alteramos un slider o input
 function createBlob() {
+    // recogemos los valores de cada slider
+    let radiusOne = sliders[0].value;
+    let radiusTwo = sliders[1].value;
+    let radiusThree = sliders[2].value;
+    let radiusFour = sliders[3].value;
 
+    // recogemos los valores numéricos de altura y anchura
+    let blobHeight = inputs[0].value;
+    let blobWidth = inputs[1].value;
+
+    // creamos una variable que reúna todos estos valores
+    let borderRadius = `${radiusOne}% ${100 - radiusOne}% ${100 - radiusThree}% ${radiusThree}% /
+    ${radiusFour}% ${radiusTwo}% ${100 - radiusTwo}% ${100 - radiusFour}%`
+
+    // escribimos el css del blob con los datos de los inputs
+    document.querySelector('.output').style.cssText = 
+    `border-radius:${borderRadius}; 
+    height:${blobHeight}px; 
+    width:${blobWidth}px;`; 
+
+    // imprimos en pantalla el valor del border-radius
+    document.querySelector('.output-code').innerHTML = `${borderRadius}`;
 }
