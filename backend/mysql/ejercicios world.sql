c7dc563d6188603e05c3ee6e96787784e0aac829
Elurr <jonatikkan@gmail.com>
2023-01-09 14:01:14 +0100

    lunes 9-01

--- backend/mysql/ejercicios world.sql
+++ backend/mysql/ejercicios world.sql
@@ -29,10 +29,34 @@ SELECT name, population FROM world.city ORDER BY Population DESC LIMIT 1;
 
 
 -- Escribir una consulta SQL que muestre la lista de todos los idiomas hablados en la región del Caribe.
+SELECT DISTINCT(language), region FROM world.countrylanguage 
+	JOIN world.country ON country.code = countrylanguage.countrycode
+    WHERE region = 'Caribbean';
+    -- DISTINCT(nombre_de_columna) es un método para que los resultados no se repitan
+
 -- Escribir una consulta SQL que muestre la capital de España.
+SELECT country.name AS País, city.name AS Capital FROM world.country 
+	JOIN world.city ON country.capital = city.id
+    WHERE country.name = 'Spain';
+    -- AS indica un alias para la columna
+    
 -- Escribir una consulta SQL que muestre el país con mayor esperanza de vida.
+SELECT name, lifeExpectancy FROM world.country
+	ORDER BY lifeExpectancy DESC LIMIT 1;
 -- Escribir una consulta SQL que muestre todas las ciudades de Europa.
--- Escribir una consulta SQL que muestre la ciudad más poblada de la tabla city.
--- Escribir una consulta SQL que muestre la ciudad menos poblada de la tabla city.
+SELECT city.name, continent FROM world.country 
+	JOIN world.city ON country.code = city.countrycode
+    WHERE continent = 'Europe';
+-- Escribir una consulta SQL que muestre las ciudades de Sudamérica con una población mayor a 500000.    
+SELECT city.name, city.Population, continent FROM world.country 
+	JOIN world.city ON country.code = city.countrycode
+    WHERE continent = 'South America'
+    AND city.Population > 5000000;    
+
 -- Escribir una consulta SQL que calcule el número de resultados de la tabla city.
+SELECT COUNT(*) FROM world.city;
+
 -- Escribir una consulta SQL que calcule el número de ciudades de China en la tabla city.
+SELECT COUNT(city.name) FROM world.country	
+	JOIN world.city ON country.code = city.countrycode
+    WHERE country.name = 'China';
