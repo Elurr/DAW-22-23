767a273e02c0cf82710c46ea38486e55b118faa4
Elurr <jonatikkan@gmail.com>
2023-01-31 13:35:41 +0100

    clase del martes 31

--- backend/Alumnos/Rebeca C/logeo_y_registro/panel-user.php
+++ backend/Alumnos/Rebeca C/logeo_y_registro/panel-user.php
@@ -9,8 +9,6 @@ if (isset($_SESSION['logged']) && $_SESSION['usertype'] == 'admin') {
     $sql = "SELECT * FROM usuarios WHERE nombre = '$user'";
 }
 $result = $conn->query($sql);
-
-
 ?>
 
 <!DOCTYPE html>
@@ -46,27 +44,39 @@ $result = $conn->query($sql);
 </head>
 
 <body>
-    <h1>Tabla de los usuarios de la BD</h1>
-    <table>
+    <form action="">
+        <h1>Tabla de los usuarios de la BD</h1>
+        <table>
 
-        <?php
-        if ($result->num_rows > 0) {
-            echo "<tr>
+            <?php
+            if ($result->num_rows > 0) {
+                echo "<tr>
             <th>nombre</th>
             <th>contraseña</th>
             <th>tipo de usuario</th>
+            <th>modificar</th>
             </tr>";
-            // imprimir los datos de cada fila
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr> <td>" . $row['nombre'] . "</td>" .
-                    "<td>" . $row['password'] . "</td>" .
-                    "<td>" . $row['user_type'] . "</td> </tr>";
-            }
-        }
+                // imprimir los datos de cada fila
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr> <td>" . $row['nombre'] . "</td>" .
+                        "<td>" . $row['password'] . "</td>" .
+                        "<td>" . $row['user_type'] . "</td> 
 
-        $conn->close();
-        ?>
-    </table>
+                        
+                        <td> <svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px'
+                        width='24' height='24'
+                        viewBox='0 0 24 24'>
+                            <path d='M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z'></path>
+                        </svg></td> 
+                        
+                        </tr>";
+                }
+            }
+
+            $conn->close();
+            ?>
+        </table>
+    </form>
 
 </body>
 
