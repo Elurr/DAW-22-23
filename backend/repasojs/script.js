18b84370f94ef86473d100766173d9cfa5e24ac4
Elurr <jonatikkan@gmail.com>
2023-02-16 13:27:26 +0100

    clase del jueves 16

--- backend/repasojs/script.js
+++ backend/repasojs/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
-    $('input.pass').on('change', function() {
+    $('input.pass').on('keyup', function() {
         // quiero comparar los valores de los dos inputs
+
+        // recogemos el valor de los inputs
         valor1 = $('input[name="pass"]').val();
         valor2 = $('input[name="confirm"]').val();
 
-        if (valor1 == valor2) {
-            // si los valores coinciden:
-
-            // activamos el botón de registro
-            $('input[type="submit"]').removeAttr('disabled');
+        // sólo se ejecutará cuando los inputs tengan la misma longitud
+        if (valor1.length == valor2.length) {
+            if (valor1 == valor2) {
+                // si los valores coinciden:
+    
+                // activamos el botón de registro
+                $('input[type="submit"]').removeAttr('disabled');
+            } else alert('las contraseñas no coinciden');
         }
     })
 });
