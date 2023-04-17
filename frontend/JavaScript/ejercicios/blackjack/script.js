beb2fea912ce4912048355e7e2bbda1eac95ec54
Elurr <jonatikkan@gmail.com>
2023-04-17 12:54:31 +0200

    voltear las cartas

--- frontend/JavaScript/ejercicios/blackjack/script.js
+++ frontend/JavaScript/ejercicios/blackjack/script.js
@@ -10,16 +10,16 @@ let iconoTreboles = `<i class="bi bi-suit-club"></i>`;
 function crearBaraja() {
     let baraja = new Array();
     let palos = {
-        "T":"Tréboles",
-        "D":"Diamantes",
-        "P":"Picas",
-        "C":"Corazones"
+        "T": "Tréboles",
+        "D": "Diamantes",
+        "P": "Picas",
+        "C": "Corazones"
     }
     let rangos = new Array("A", 2, 3, 4, 5, 6, 7, 8, 9, 0, "J", "Q", "K");
 
-    Object.keys(palos).forEach(function(value) {
+    Object.keys(palos).forEach(function (value) {
         for (let i = 0; i < rangos.length; i++) {
-            
+
             baraja.push(rangos[i] + value);
         }
     })
@@ -34,42 +34,67 @@ function crearBaraja() {
         switch (palo) {
             case 'T':
                 print = iconoTreboles;
-                color = '#069FC5'; 
+                color = 'carta-negra';
                 break;
             case 'D':
                 print = iconoDiamantes;
-                color = '#c52c06'; 
+                color = 'carta-roja';
                 break;
             case 'C':
                 print = iconoCorazones;
-                color = '#c52c06'; 
+                color = 'carta-roja';
                 break;
             case 'P':
                 print = iconoPicas;
-                color = '#069FC5'; 
+                color = 'carta-negra';
                 break;
-        
+
             default:
                 break;
         }
 
-        contenedorBaraja.innerHTML += 
-        "<div class='carta baraja' style='color:"+ color +"'>"
-        + "<div class='num top'>" + valor + "</div>" 
-        + "<div class='palo'>" + print + "</div>"
-        + "<div class='num bot'>" + valor + "</div>" 
-        + "</div>";
+        contenedorBaraja.innerHTML +=
+            "<div id='" + baraja[i] + "' class='carta baraja'>"
+            + "<div class='carta-contenedor'>"
+            + "<div class='frontal " + color + "'>"
+            + "<div class='num top'>" + valor + "</div>"
+            + "<div class='palo'>" + print + "</div>"
+            + "<div class='num bot'>" + valor + "</div>"
+            + "</div>"
+
+            + "<div class='duda trasera'><div class='palo'>" + iconoDuda + "</div></div>"
+            + "</div>"
+            + "</div>";
 
         let cartaTop = document.querySelectorAll('.baraja');
         cartaTop[cartaTop.length - 1].style.zIndex = i;
-        cartaTop[cartaTop.length - 1].style.boxShadow = "0 0 2px 1px #eff";
-        // cartaTop[cartaTop.length - 1].style.position = "absolute";
-        // cartaTop[cartaTop.length - 1].style.left = i/3+"px";
-        // cartaTop[cartaTop.length - 1].style.top = i+"px";
+        // cartaTop[cartaTop.length - 1].style.boxShadow = "0 0 2px 1px #eff";
+
     }
 }
 crearBaraja();
+let baraja = document.querySelectorAll('.baraja');
+let caraCartas = document.querySelectorAll('.baraja .carta-contenedor');
+let barajaTrasera = document.querySelectorAll('.trasera');
+let num = document.querySelectorAll('.baraja .num');
+let palos = document.querySelectorAll('.baraja .palo');
 
+function voltearBaraja() {
+
+    if (caraCartas[0].classList.contains('voltear')) {
+        for (let i = 0; i < caraCartas.length; i++) {
+            caraCartas[i].classList.remove('voltear');
+        }
+    } else {
+        for (let i = 0; i < caraCartas.length; i++) {
+            caraCartas[i].classList.add('voltear');
+        }
+    }
+}
+
+function juntar() {
+
+}
 
 // El juego de la casa
 let puntosCasa = 0;
@@ -132,16 +157,16 @@ function empezarJuego() {
     // fin = false;
     activarBotones();
 
-    
+
     // Recogemos la dos cartas iniciales del jugador:
     darCarta();
     darCarta();
 
     // Recogemos la dos cartas iniciales de la casa:
     darCarta("casa");
-    manoCasa.innerHTML += "<div class='carta duda'>" 
-    + "<div class='palo'>" + iconoDuda + "</div>"
-    + "</div>";
+    manoCasa.innerHTML += "<div class='carta duda'>"
+        + "<div class='palo'>" + iconoDuda + "</div>"
+        + "</div>";
 
     // Cada segundo los tiempos de la animación del parpadeo se randomizan
     let tempParp = setInterval(parpadeo, 1000);
@@ -228,23 +253,23 @@ function mostrarCartas() {
     manoCasa.innerHTML = '';
     manoJugador.innerHTML = '';
     for (let i = 0; i < jugadaCasa.length; i++) {
-       manoCasa.innerHTML += "<div class='carta'>"
-       + "<div class='num top'>" + jugadaCasa[i] + "</div>" 
-       + "<div class='palo'>" + iconoDiamantes + "</div>"
-       + "<div class='num bot'>" + jugadaCasa[i] + "</div>" 
-       + "</div>";
+        manoCasa.innerHTML += "<div class='carta'>"
+            + "<div class='num top'>" + jugadaCasa[i] + "</div>"
+            + "<div class='palo'>" + iconoDiamantes + "</div>"
+            + "<div class='num bot'>" + jugadaCasa[i] + "</div>"
+            + "</div>";
     }
     for (let i = 0; i < jugadaJugador.length; i++) {
-       manoJugador.innerHTML += "<div class='carta'>"
-       + "<div class='num top'>" + jugadaJugador[i] + "</div>" 
-       + "<div class='palo'>" + iconoPicas + "</div>"
-       + "<div class='num bot'>" + jugadaJugador[i] + "</div>" 
-       + "</div>";
+        manoJugador.innerHTML += "<div class='carta'>"
+            + "<div class='num top'>" + jugadaJugador[i] + "</div>"
+            + "<div class='palo'>" + iconoPicas + "</div>"
+            + "<div class='num bot'>" + jugadaJugador[i] + "</div>"
+            + "</div>";
     }
 }
 
 function ganador() {
-    
+
     // Switch para comentar el estado actual del juego
     switch (true) {
         case puntosJugador > puntosCasa:
@@ -258,7 +283,7 @@ function ganador() {
         case puntosCasa === puntosJugador:
             resultado.innerHTML = "Hay empate";
             break;
-    
+
         default:
             console.log("default switch 1");
             break;
@@ -271,28 +296,28 @@ function ganador() {
         case (puntosCasa === 21 && puntosJugador === 21):
             resultado.innerHTML = "Ambas partes tienen 21, la apuesta se recupera."
             break;
-    
+
         case (puntosCasa === 21 && puntosJugador != 21):
-            resultado.innerHTML = "La casa tiene un Blackjack. El jugador pierde la apuesta";   
+            resultado.innerHTML = "La casa tiene un Blackjack. El jugador pierde la apuesta";
             break;
-            
+
         case (puntosCasa != 21 && puntosJugador === 21):
-            resultado.innerHTML = "El jugador tiene un Blackjack. Ha ganado!"; 
+            resultado.innerHTML = "El jugador tiene un Blackjack. Ha ganado!";
             break;
 
         case puntosJugador > 21:
             resultado.innerHTML = "El jugador se ha pasado de 21. Gana la casa";
-            break; 
+            break;
 
         case puntosCasa > 21:
             resultado.innerHTML = "La casa se ha pasado de 21. Gana el jugador";
-            break; 
+            break;
 
         default:
             console.log("default switch 2");
             activarBotones();
             break;
-    }   
+    }
 }
 
 function darCarta(jugada) {
@@ -321,7 +346,7 @@ function plantarse() {
         clearTimeout(temporizador);
         temporizador = 0;
     }
-    
+
 }
 
 // Animación del parpadeo de la luz de neón
@@ -329,7 +354,7 @@ function parpadeo() {
     let luces = document.querySelectorAll('.carta:hover div');
     for (let i = 0; i < luces.length; i++) {
         luces[i].style.animationDuration = Math.floor(Math.random() * 10 + 1) + "s";
-        
+
     }
 }
 
@@ -347,5 +372,16 @@ btnPlantarse.addEventListener('click', desactivarBotones);
 // let arrayCartas = [];
 // for (let i = 0; i < arrayCartas.length; i++) {
 //     arrayCartas[i].style.zIndex = i;
-    
+
 // }
+
+
+// German
+
+let cartas = document.querySelectorAll('.card');
+
+for (let i = 0; i < cartas.length; i++) {
+    cartas[i].classList.remove('card');
+
+}
+
